8ee209a68d0ca19b51db535c79ed227837948318
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-12-03 14:38:30 +0100

    Bundle

---
+++ src/main/java/fr/esgi/cupcake/Bundle.java
@@ -0,0 +1,11 @@
+package fr.esgi.cupcake;
+
+import java.util.List;
+
+public class Bundle {
+    List<Cake> listOfCake;
+
+    public Bundle(List<Cake> listOfCake) {
+        this.listOfCake = listOfCake;
+    }
+}
