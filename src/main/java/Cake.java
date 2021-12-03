1f1c33083b13485512280974159ec5b5d9382e5c
Mohamed Mili <muhamed.mili19@gmail.com>
2021-12-03 14:33:56 +0100

    ADDED CAKE

---
+++ src/main/java/Cake.java
@@ -0,0 +1,29 @@
+public class Cake {
+    private String Nom;
+    private float Prix;
+
+
+    public Cake(String nom, float prix) {
+        Nom = nom;
+        Prix = prix;
+
+    }
+
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public float getPrix() {
+        return Prix;
+    }
+
+    public void setPrix(float prix) {
+        Prix = prix;
+    }
+
+}
